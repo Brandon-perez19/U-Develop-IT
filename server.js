5e02016c01377f1684078cae644a59129c5c5838
Brandon <brandonperez@brandons-mbp.lan>
2022-05-03 17:06:46 -0400

    added creat, delete and return functions from database to server

--- server.js
+++ server.js
@@ -1,10 +1,60 @@
 const express = require('express');
+const mysql = require('mysql2');
 const PORT = process.env.PORT|| 3001;
 const app = express();
 
 //express middleware
 app.unsubscribe(express.urlencoded({ extended: false}));
 app.use(express.json());
+
+//connect to database
+const db = mysql.createConnection(
+
+    {
+        host: 'localhost',
+
+        //my MySQL username
+        user: 'root',
+
+        //my MySQL password
+        password: '',
+        database: 'election'
+    },
+
+    console.log('Connected to the election database.')
+);
+// // Returns all rows
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//      console.log(rows);
+// });
+
+// // returns single candidate
+// db.query(`SELECT * FROM candidates WHERE id=1`, (err, row) => {
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
+
+// // Delete a candidate (Preparred Statement). ? is a placeholder and 1 is the value to use in place of the placeholder
+// db.query(`DELETE FROM candidates WHERE id =?`, 1, (err, result) => {
+//     if (err){
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+//create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+                VALUES (?,?,?,?)`;
+const params = [1, 'Ronald','Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+    if (err){
+        console.log(err);
+    }
+    console.log(result);
+});
 
 app.use((req, res) => {
 
@@ -13,5 +63,5 @@ app.use((req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log('Server running on port ${PORT}');
+    console.log(`Server running on port ${PORT}`);
 });
