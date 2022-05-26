a514ba7926d8e2e841f16dee51bb6c79d401925f
Brandon <brandonperez@brandons-mbp.lan>
2022-05-26 18:41:33 -0400

    connected server to database.

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mysql = require('mysql2');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -6,12 +7,28 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//connect to the database
+const db =mysql.createConnection(
+    {
+        host: 'localhost',
+        //mysql username
+        user: 'root',
+        //mysql password
+        password: '',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
 //test route 
 // app.get('/', (req,res) => {
 //     res.json( {
 //         message: "Hello World!"
 //     });
 // });
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 //Default response for any other request (Not Found)
 app.use((req,res) => {
