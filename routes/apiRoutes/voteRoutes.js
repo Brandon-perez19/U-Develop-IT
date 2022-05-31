45b1d3704c3b3549106613ecf50cf05ac0c375e4
Brandon <brandonperez@brandons-mbp.lan>
2022-05-31 15:21:28 -0400

    able to retrieve results in descending order and grouped by candidate with party description.:

---
+++ routes/apiRoutes/voteRoutes.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../../db/connection');
+const inputCheck = require('../../utils/inputCheck');
+
+router.post('/vote', ({ body }, res) => {
+    // Data validation
+    const errors = inputCheck(body, 'voter_id', 'candidate_id');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+
+    const sql = `INSERT INTO votes (voter_id, candidate_id) VALUES (?,?)`;
+    const params = [body.voter_id, body.candidate_id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body,
+            changes: result.affectedRows
+        });
+    });
+});
+
+router.get('/votes', ( {body} , res) => {
+    const sql = `SELECT candidates.*, parties.name AS party_name, COUNT(candidate_id) AS count
+                FROM votes
+                LEFT JOIN candidates ON votes.candidate_id = candidates.id
+                LEFT JOIN parties ON candidates.party_id = parties.id
+                GROUP BY candidate_id ORDER BY count DESC; `
+
+    const params = [body.candidate_id, body.voter_id];
+    db.query(sql, params, (err, rows) => {
+        if(err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'succes',
+            data: rows
+        })
+    })
+})
+
+module.exports = router;
